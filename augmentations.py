7918e459129d6a023804c8c02b499efa0f0983b0
Victoria Shevchenko <victoria.shevchenko@ens.psl.eu>
2024-04-30 11:06:29 +0200

    edge flipping aug fixed

--- augmentations.py
+++ augmentations.py
@@ -2,6 +2,7 @@ import numpy as np
 from nilearn.connectome import sym_matrix_to_vec, vec_to_sym_matrix
 from scipy.linalg import pinv, diagsvd
 from sklearn.utils.extmath import randomized_svd
+from itertools import combinations
 
 def threshMat(conn, thresh):
     perc = np.percentile(np.abs(conn), thresh, axis=1)  # Calculate the percentile along each matrix
@@ -17,31 +18,30 @@ def random_threshold_augmentation(features, threshold):
 #     normalized_features = augmented_features / norm if norm != 0 else augmented_features
     return augmented_features
 
-def flipping_threshold_augmentation(features, threshold, max_attempts, hemisphere_size=None):
-    features_thresholded, _ = threshMat(features, threshold)
-    zero_indices = np.argwhere(features_thresholded == 0)
-    print(zero_indices.shape)
+matrix = np.load("matrices.npy")[0]
+
+def flipping_threshold_augmentation(matrix, threshold, hemisphere_size=None):
+    matrix_thresholded, _ = threshMat(matrix, threshold)
     
     if hemisphere_size is None:
-        hemisphere_size = features.shape[0] // 2
+        hemisphere_size = 500
+        
+    reg_indices_left = np.arange(0, hemisphere_size)
+    reg_indices_right = np.arange(hemisphere_size, 2*hemisphere_size)
     
-    attempt = 0
-    max_attempts = max_attempts  
+    edge_indices_left = list(combinations(reg_indices_left, 2))
+    edge_indices_right = list(combinations(reg_indices_right, 2))
     
-    while attempt < max_attempts:
-            selected_idx = zero_indices[np.random.choice(zero_indices.shape[0])]
-            region1 = selected_idx[0]
-            opposite_region = region1 + hemisphere_size if region1 < hemisphere_size else region1 - hemisphere_size
+    flip_index_pairs = []
+    for edge_left, edge_right in zip(edge_indices_left, edge_indices_right):
+        if (matrix_thresholded[edge_left] == 0) & (matrix_thresholded[edge_right] == 0):
+            flip_index_pairs.append([edge_left, edge_right])
             
-            if features_thresholded[opposite_region, selected_idx[1]] == 0:
-                selected_pairs = [[region1, opposite_region]]
-                flipped_matrices = flip_edge_between_regions(features, selected_pairs)
-            else:
-                print(f"Attempt {attempt}: no valid pair found.")
-                flipped_matrices = None
-            attempt += 1
+    print(len(flip_index_pairs))                          
+    flipped_matrix = flip_edges(matrix, flip_index_pairs)
+            
 
-    return flipped_matrices
+    return flipped_matrix, flip_index_pairs
 
 def SVD_augmentation(X, n_components, n_iter, noise_factor, random_state=42):
     X_pinv = pinv(X)
@@ -53,39 +53,26 @@ def SVD_augmentation(X, n_components, n_iter, noise_factor, random_state=42):
     X_augmented = np.dot(original_V, np.dot(np.diag(original_Sigma), original_U))
     return X_augmented
 
-def flip_edge_between_regions(matrix, regions, vectorize_mat=True):
-    if not isinstance(regions[0], list):
-        regions = [regions]
-    flipped_matrices = []
+def flip_edges(matrix, edge_idx, vectorize_mat=False):
     
-    for pair in regions:
-        new_matrix = matrix.copy()
-        hemisphere_size = matrix.shape[0] // 2
-        
-        # Determine the indices for flipping
-        for index1, index2 in [pair]:
-            is_left_hemisphere1 = index1 < hemisphere_size
-            is_left_hemisphere2 = index2 < hemisphere_size
+    if not isinstance(edge_idx[0], type([])):
+        edge_idx = [edge_idx]
+    flipped_matrix = matrix.copy()
+    for i, _ in enumerate(edge_idx):
+        edge_pair = edge_idx[i]
+        indices_left = edge_pair[0]
+        indices_right = edge_pair[1]
+        edge_left = matrix[indices_left]
+        edge_right = matrix[indices_right]
             
-            # Calculate the opposite indices in the other hemisphere
-            #opposite_index1 = index1 + (-1 if is_left_hemisphere1 else 1) * hemisphere_size
-            #opposite_index2 = index2 + (-1 if is_left_hemisphere2 else 1) * hemisphere_size
-            opposite_index1 = index1 + (1 if is_left_hemisphere1 else -1) * hemisphere_size
-            opposite_index2 = index2 + (1 if is_left_hemisphere2 else -1) * hemisphere_size
-
-            # Flip the connectivity edge between the specified pairs across hemispheres
-            new_matrix[index1, opposite_index2], new_matrix[index2, opposite_index1] = \
-                new_matrix[index2, opposite_index1], new_matrix[index1, opposite_index2]
-            new_matrix[opposite_index2, index1], new_matrix[opposite_index1, index2] = \
-                new_matrix[opposite_index1, index2], new_matrix[opposite_index2, index1]
+        flipped_matrix[indices_left] = edge_right
+        flipped_matrix[indices_right] = edge_left
         
-        if vectorize_mat:
-            # Assuming 'sym_matrix_to_vec' is a function you have that vectorizes a symmetric matrix
-            new_matrix = sym_matrix_to_vec(new_matrix, discard_diagonal=True)
+    if vectorize_mat:
+        flipped_matrix = sym_matrix_to_vec(flipped_matrix, discard_diagonal=True)
         
-        flipped_matrices.append(new_matrix)
         
-        return np.array(flipped_matrices)
+    return flipped_matrix
 
 augs = {
     "random_threshold_augmentation": random_threshold_augmentation,
@@ -95,6 +82,6 @@ augs = {
 
 aug_args = {
     "random_threshold_augmentation": {"threshold": 60},
-    "flipping_threshold_augmentation": {"threshold": 60, "max_attempts" : 100, "hemisphere_size": None},
+    "flipping_threshold_augmentation": {"threshold": 60, "hemisphere_size": None},
     "SVD_augmentation": {"n_components": 10, "n_iter": 5, "noise_factor": 0.01, "random_state":42}
 }
