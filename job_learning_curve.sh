b594f8eb78eef5084e1685c700e9f04945e80753
Victoria Shevchenko <shevchenko682@gmail.com>
2024-07-25 13:31:45 +0100

    full model working w/ abcd; good recon

---
+++ job_learning_curve.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+#SBATCH --job-name=LCurve
+#SBATCH -o ./logs/LCurve-%j.out
+#SBATCH -p gpu_long
+#SBATCH --gpus-per-node 1
+
+module load Python/3.9.6-GCCcore-11.2.0
+source /well/margulies/users/cpy397/env/ClinicalGrads/bin/activate
+echo the job id is $SLURM_JOB_ID
+
+python3 -u lc_multivariate_abcd.py
