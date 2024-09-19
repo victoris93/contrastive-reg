eb910c95d757b9f94f2a6016b4f0c95fcc9f3ff8
victoris93 <shevchenko682@gmail.com>
2024-09-19 08:00:03 +0100

    updates to autoencoders

--- mat_autoencoder.py
+++ mat_autoencoder.py
@@ -32,56 +32,13 @@ from ContModeling.modeling import test_mat_autoencoder, train_mat_autoencoder
 from ContModeling.utils import get_best_fold, mape_between_subjects, mean_correlations_between_subjects
 from ContModeling.losses import LogEuclideanLoss, NormLoss
 from ContModeling.models import MatAutoEncoder
-from ContModeling.helper_classes import MatData
+from ContModeling.helper_classes import MatData, FoldTrain
 
 print("Fetching device...")
 torch.cuda.empty_cache()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 #Input to the train autoencoder function is train_dataset.dataset.matrices
-
-class FoldTrain(submitit.helpers.Checkpointable):
-    def __init__(self):
-        self.results = None
-
-    def __call__(self, fold, train_idx, val_idx, train_dataset, model_params_dir, cfg, random_state=None, device=None, path: Path = None):
-
-        if self.results is None:
-            if device is None:
-                device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            if not isinstance(random_state, np.random.RandomState):
-                random_state = np.random.RandomState(random_state)
-        
-        self.fold = fold + 1
-    
-        input_dim_feat=cfg.input_dim_feat
-        output_dim_feat=cfg.output_dim_feat
-
-
-        print(f"Fold {self.fold}")
-        fold_train_dataset = Subset(train_dataset, train_idx)
-        fold_val_dataset = Subset(train_dataset, val_idx)
-        
-        train_features = torch.stack([fold_train_dataset[i][0] for i in range(len(fold_train_dataset))])
-        mean_f = torch.mean(torch.tensor(train_features), dim=0).to(device)
-        [D,V] = torch.linalg.eigh(mean_f,UPLO = "U")     
-        B_init_fMRI = V[:,input_dim_feat-output_dim_feat:]
-    
-        loss_terms, trained_weights, val_loss = train_mat_autoencoder(self.fold, fold_train_dataset, fold_val_dataset, B_init_fMRI, cfg, device)
-        torch.save(trained_weights, f"{model_params_dir}/autoencoder_weights_fold{self.fold}.pth")
-        
-        self.results = {"fold": self.fold,
-                        "val_loss": val_loss,
-                        } 
-        return self.results
-
-    def checkpoint(self, *args, **kwargs):
-        print("Checkpointing", flush=True)
-        return super().checkpoint(*args, **kwargs)
-
-    def save(self, path: Path):
-        with open(path, "wb") as o:
-            pickle.dump(self.results, o, pickle.HIGHEST_PROTOCOL)
 
 # +
 @hydra.main(config_path=".", config_name="mat_autoencoder_config")
@@ -127,7 +84,7 @@ def main(cfg: DictConfig):
         with executor.batch():
             for fold, (train_idx, val_idx) in enumerate(kf.split(train_val_idx)):
                 run_train_fold = FoldTrain()
-                job = executor.submit(run_train_fold, fold, train_idx, val_idx, train_val_dataset, model_params_dir = model_params_dir, cfg =cfg, random_state = random_state)
+                job = executor.submit(run_train_fold, fold, train_mat_autoencoder, train_idx, val_idx, train_val_dataset, model_params_dir = model_params_dir, cfg =cfg, random_state = random_state)
                 fold_jobs.append(job)
 
         async def get_result(fold_jobs):
@@ -141,7 +98,7 @@ def main(cfg: DictConfig):
         fold_results = []
         for fold, (train_idx, val_idx) in enumerate(kf.split(train_val_idx)):
             run_train_fold = FoldTrain()
-            job = run_train_fold(fold, train_idx, val_idx, train_val_dataset, random_state = random_state, model_params_dir = model_params_dir, cfg = cfg)
+            job = run_train_fold(fold, train_mat_autoencoder, train_idx, val_idx, train_val_dataset, model_params_dir = model_params_dir, cfg =cfg, random_state = random_state)
             fold_results.append(job)
     # TEST
     executor = submitit.AutoExecutor(folder=str(Path("./logs") / "%j"))
