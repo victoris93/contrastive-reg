a90d0d5478d9d5e1835b43b5ecad920549ff286f
victoris93 <shevchenko682@gmail.com>
2024-11-18 14:16:32 +0100

    bug fix in neighborhood var

--- compute_var_neighborhood.py
+++ compute_var_neighborhood.py
@@ -16,31 +16,34 @@ predictions = predictions[(predictions["dataset"] == dataset) & (predictions["tr
 sub_idx = predictions.indices.values
 embeddings = np.load(f"{exp_dir}/embeddings/joint_embeddings_run0_{dataset}.npy")
 n_neighbors = int(sys.argv[3])
+step = int(sys.argv[4])
 
 neigh = NearestNeighbors(n_neighbors=n_neighbors, radius=1, algorithm='brute', metric = 'cosine', p = 2).fit(embeddings)
 distances, neighbors = neigh.kneighbors(embeddings, return_distance = True)
 data =  xr.open_dataset(f'{root}/data/abcd_dataset_400parcels_1.nc')
 
 neigh_conn_var = []
-neigh_conn_dvar_ddist = []
 
-for neighborhood_idx, neighborhood in enumerate(tqdm(neighbors)):
+for neighborhood in tqdm(neighbors[0::step]):
+    
+    neighborhood_idx = neighborhood[0]
+    neigh_conn_dvar_ddist = []
     var = 0
     dist_neigh = distances[neighborhood_idx]
-    neigh_idx = sub_idx[neighborhood]
-    neigh_matrices = data.matrices.isel(subject = neigh_idx).values
+    neigh_indices= sub_idx[neighborhood]
+    neigh_matrices = data.matrices.isel(subject = neigh_indices).values
     centroid = neigh_matrices[0]
 
     for mat_idx, mat in enumerate(neigh_matrices):
-        if mat_idx == 0:
-            continue
-        var_shift = (centroid - mat) ** 2
-        var += var_shift
-        sub_dvar_ddist = var_shift / dist_neigh[mat_idx]
-        neigh_conn_dvar_ddist.append(sub_dvar_ddist)
+        if mat_idx != 0:
+            print("mat_idx: ", mat_idx)
+            var_shift = (centroid - mat) ** 2
+            var += var_shift
+            sub_dvar_ddist = var_shift / dist_neigh[mat_idx]
+            neigh_conn_dvar_ddist.append(sub_dvar_ddist)
     
     neigh_conn_var.append(var / len(neighborhood[1:]))
-    np.save(f"{exp_dir}/embeddings/neigh_conn_dvar_ddist_{dataset}_neigh_idx{neighborhood_idx}.npy", np.array(neigh_conn_dvar_ddist))
+    np.save(f"{exp_dir}/embeddings/neigh_conn_dvar_ddist_{dataset}_neighborhood{neighborhood_idx}.npy", np.array(neigh_conn_dvar_ddist))
 
 neigh_conn_var = np.array(neigh_conn_var)
 np.save(f"{exp_dir}/embeddings/neigh_conn_var_{dataset}.npy", neigh_conn_var)
