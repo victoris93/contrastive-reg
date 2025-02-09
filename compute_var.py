16a21649d77719bae4bba0b4b7334ae5faf98ff4
Victoria Shevchenko <shevchenko682@gmail.com>
2025-02-09 17:53:53 +0100

    compute matrix var as distance from reference subject

--- compute_var.py
+++ compute_var.py
@@ -16,12 +16,17 @@ exp_dir = f'{root}/results/{exp_name}'
 predictions = pd.read_csv(f'{exp_dir}/pred_results.csv')
 predictions = predictions[(predictions["dataset"] == dataset) & (predictions["train_ratio"] == 1) & (predictions["model_run"] == 0)]
 sub_idx = predictions.indices.values
+matrices = data.isel(index = sub_idx).matrices.values
+
+ref_mat = matrices[0]
 
 vars = []
 for idx in sub_idx:
-    mat = data.matrices.isel(index = idx).values
-    var = np.var(mat)
-    vars.append(var)
+    if idx != 0:
+        mat = data.matrices.isel(index = idx).values
+        var_shift = (ref_mat - mat) ** 2
+        vars.append(var_shift)
+
 vars = np.array(vars)
 np.save(f"{exp_dir}/embeddings/conn_var_{dataset}.npy", vars)
 print(f"Variance of connectivity matrices for {exp_name} is computed and saved.")
